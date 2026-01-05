83efe20ef42dc207e2e9f1168485c385a8139df0
timothyoghenetega5-cpu <timothyoghenetega5@gmail.com>
2026-01-05 18:01:34 +0100

    Hair brand 1

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+	const menu = document.querySelector(".menu-links");
+	const icon = document.querySelector(".hamburger-icon");
+
+	menu.classList.toggle("open");
+	icon.classList.toggle("open");
+}
+
+document.querySelector(".hamburger-icon").addEventListener("click", toggleMenu);
